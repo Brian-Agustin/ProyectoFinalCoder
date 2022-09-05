ab383e7074ed4480e7ce0c1127641d23c941ee38
Pedro Leon Kalil Suarez <peleoka12345@gmail.com>
2022-09-05 19:15:38 -0300

    firstCommit

--- AppLente/models.py
+++ AppLente/models.py
@@ -12,7 +12,7 @@ class Turno(models.Model):
     numero = models.IntegerField()
     correo = models.EmailField()
     dni = models.IntegerField()
-    fecha = models.IntegerField(max_length=50)
+    fecha = models.IntegerField()
     def __str__(self):
         return f"Nombre: {self.nombre}, Numero: {self.numero}, Correo: {self.correo}, dni: {self.dni}, fecha: {self.fecha}"
 class Cotiza(models.Model):
