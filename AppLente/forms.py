b0339aa8bc442bbfc4b4c9bb9d28da267de67cb4
Brian-Agustin <braiian.dlv@gmail.com>
2022-09-30 20:37:04 -0300

    add choices cotization

--- AppLente/forms.py
+++ AppLente/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class Contactof(forms.Form):
     nombre = forms.CharField(max_length=40)
     numero = forms.IntegerField()
-    correo = forms.EmailField()
+    correo = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'ex@example.com'}))
     mensaje = forms.CharField(max_length=50)
 
 class Turnof(forms.Form):
@@ -14,9 +14,16 @@ class Turnof(forms.Form):
     dni = forms.CharField(max_length=20)
     fecha = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
 
+
+sk_import =(
+    ("ninguno", "Ninguno seleccionado"),
+    ("reparacion", "Reparacion / restauracion"),
+    ("compras", "Compras"),
+    ("envios", "Envios"),
+)
 class Cotizaf(forms.Form):
-    servicio = forms.CharField(max_length=50)
-    mensaje = forms.CharField(max_length=50)
+    servicio = forms.ChoiceField(choices=sk_import)
+    mensaje = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'placeholder': 'Escriba aqui'}))
 
 class BuscaTurnoPorDNI(forms.Form):
     dni = forms.CharField(max_length=20)
